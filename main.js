a96fde5bc3d7ac5f86f04c8d2f72cb76a8f4a430
Phatira Bruce <phatirabruce@Phatiras-MacBook-Air.local>
2018-01-09 20:31:54 -0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,1 @@
+alert('Yo gorons sup welcome to teh tcoh site developed by the propogandist if you see this message you did everything correctly an u officially have the tcoh site downloaded an ready to view')
